81fbf62e73130a3eba578d0055e86725fd275907
1wwg2 <vigleg777@gmail.com>
2023-03-28 17:51:27 +0300

    Update ex10.cpp

--- Chapter_4/Ex_X/ex10.cpp
+++ Chapter_4/Ex_X/ex10.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main() {
+    array<double, 3> Arr;
+    cout << "Enter three times for the 40-meter dash\n 1: ";
+    cin >> Arr[0];
+    cout << " 2: ";
+    cin >> Arr[1];
+    cout << " 3: ";
+    cin >> Arr[2];
+    cout << "Medium value: " << (Arr[0] + Arr[1] + Arr[2]) / 3 << endl;
+
+
+    return 0;
+}
 
