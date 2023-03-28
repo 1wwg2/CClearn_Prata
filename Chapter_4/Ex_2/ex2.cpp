53327fc800618668e649945848dbad26751aba1c
1wwg2 <105918939+1wwg2@users.noreply.github.com>
2023-03-28 15:23:25 +0300

    Update ex2.cpp

--- Chapter_4/Ex_2/ex2.cpp
+++ Chapter_4/Ex_2/ex2.cpp
@@ -1,1 +1,35 @@
+/*-------------------------------------------------------
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+   
+    const int ArrSize = 20;
+    char Name [ArrSize];
+    char dessert[ArrSize];
+    cout << "What`s your name?: ";
+    cin.getline(Name, ArrSize);
+    cout << "Enter yuor favourite dessert?: ";
+    cin.getline(dessert, ArrSize);
+    cout << " I have some delicious " << dessert;
+    cout << " for you, " << Name;
 
+    return 0;
+}
+------------------------------------------------------*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string Name;
+    string dessert;
+    cout << "What`s your name?: ";
+    getline(cin, Name);
+    cout << "Enter yuor favourite dessert?: ";
+    getline(cin, dessert);
+    cout << " I have some delicious " << dessert;
+    cout << " for you, " << Name;
+
+    return 0;
+}
