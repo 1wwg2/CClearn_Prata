f75abca9c6e429d6d9db221ae05c7e81e5cd6550
1wwg2 <vigleg777@gmail.com>
2023-03-28 16:12:35 +0300

    Update ex4.cpp

--- Chapter_4/Ex_4/ex4.cpp
+++ Chapter_4/Ex_4/ex4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< Updated upstream
 
 #include <string>
 using namespace std;
@@ -16,4 +17,22 @@ int main() {
 
     return 0;
 }
+=======
+>>>>>>> Stashed changes
 
+#include <string>
+using namespace std;
+int main() {
+
+    string FirstName, LastName;
+    cout << "Enter your First name: ";
+    getline(cin, FirstName);
+    cout << "Enter your Last name: ";
+    getline(cin, LastName);
+
+    FirstName += ", " + LastName;
+    cout << "Here's the information in a single string : " << FirstName;
+
+
+    return 0;
+}
