7cea85c85cca55cebe9ebf1752c063070b04ccfb
1wwg2 <vigleg777@gmail.com>
2023-03-28 15:58:23 +0300

    Update ex3.cpp

--- Chapter_4/Ex_3/ex3.cpp
+++ Chapter_4/Ex_3/ex3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+int main() {
+    const int SizeArr = 50;
+    char FirstName[SizeArr], LastName[SizeArr];
+    cout << "Enter your First name: ";
+    cin.getline(FirstName, SizeArr);
+    cout << "Enter your Last name: ";
+    cin.getline(LastName, SizeArr);
+    strcat_s(FirstName, ", ");
+    strcat_s(FirstName, LastName);
+    cout << "Here's the information in the single string : " << FirstName;
+
+
+    return 0;
+}
