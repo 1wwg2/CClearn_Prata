5851bd889b23651e7dee38ea11588c29fba2c539
1wwg2 <105918939+1wwg2@users.noreply.github.com>
2023-03-28 16:06:47 +0300

    Update ex4.cpp

--- Chapter_4/Ex_4/ex4.cpp
+++ Chapter_4/Ex_4/ex4.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+#include <string>
+using namespace std;
+int main() {
+
+    string FirstName, LastName;
+    cout << "Enter your First name: ";
+    getline(cin, FirstName);
+    cout << "Enter your Last name: ";
+    getline(cin, LastName);
+
+    FirstName += ", " + LastName;
+    cout << "Here's the information in a single string : " << FirstName;
+
+
+    return 0;
+}
 
