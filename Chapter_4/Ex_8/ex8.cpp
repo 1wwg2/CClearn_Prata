736f012099fc22648acc03c21d29a70ba5eed963
1wwg2 <vigleg777@gmail.com>
2023-03-28 17:38:41 +0300

    Update ex8.cpp

--- Chapter_4/Ex_8/ex8.cpp
+++ Chapter_4/Ex_8/ex8.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Candybar {
+
+    string Name;
+    double Diametr = 0;
+    double Value = 0;
+
+
+};
+int main() {
+
+    Candybar* PizzaHut = new Candybar;
+    cout << "Enter diametr: ";
+    cin >> PizzaHut->Diametr;
+
+
+    cout << "Enter Name: ";
+    getline(cin, PizzaHut->Name);
+
+    cout << "Enter Value: ";
+    cin >> PizzaHut->Value;
+
+    cout << PizzaHut->Name << " " << PizzaHut->Diametr << " " << PizzaHut->Value;
+
+
+
+    return 0;
+}
+
 
