c088ad4430194c3786496bdc455c3130c2de4e4d
1wwg2 <vigleg777@gmail.com>
2023-03-31 20:54:13 +0300

    sol

--- Chapter_5/sol5ex05.cpp
+++ Chapter_5/sol5ex05.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+    int CoutSell = 0;
+    int Sum = 0;
+    string Array[] =
+    {
+     "January", "February", "March",
+     "April"  , "May"     , "June" ,
+     "July"   , "August"  , "September",
+     "October", "November", "December"
+    };
+
+    for (int i = 0; i < 12; i++) {
+
+        cout << i + 1 << ". " << Array[i] << ": ";
+        cin >> CoutSell;
+        Sum += CoutSell;
+    }
+    cout << Sum;
+
+    return 0;
+}
--- Chapter_5/sol5ex06.cpp
+++ Chapter_5/sol5ex06.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int Sum = 0;
+    int CoutSell[3][12]{};
+    string Years[] = {
+        "First",
+        "Second",
+        "Third"
+    };
+    string Array[] =
+    {
+     "January", "February", "March",
+     "April"  , "May"     , "June" ,
+     "July"   , "August"  , "September",
+     "October", "November", "December"
+    };
+
+    for (int i = 0; i < 3; i++) {
+        cout << "=======" << Years[i] << " year" << "=======" << endl;
+        for (int j = 0; j < 12; j++) {
+
+            cout << j + 1 << ". " << Array[j] << ": ";
+
+            cin >> CoutSell[i][j];
+            Sum += CoutSell[i][j];
+
+        }
+        cout << Sum << " of the " << Years[i] << " year" << endl;
+    }
+    cout << "===================================" << endl;
+    cout << "Total sum : " << Sum;
+
+    return 0;
+}
 
