103e97005897f90d8096c12f3714918d839f833b
1wwg2 <vigleg777@gmail.com>
2023-06-27 15:04:11 +0300

    sol

--- Chapter_16/sol16-08.cpp
+++ Chapter_16/sol16-08.cpp
@@ -14,10 +14,8 @@ int main()
 	cin >> FriendsNum;
 	cin.ignore();
 	vector<string>NamePet = Name(FriendsNum);
-
 	vector<string>Final(NamePet.size() + NameMet.size());
 	merge(NamePet.begin(), NamePet.end(), NameMet.begin(), NameMet.end(), Final.begin());
-	sort(Final.begin(), Final.end());
 	Final.erase(unique(Final.begin(), Final.end()), Final.end());
 
 
